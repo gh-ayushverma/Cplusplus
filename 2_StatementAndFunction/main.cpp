96aa2e63a6ce4178807f657ce6747e3b682265d5
Ayush Kumar Verma <ayushkumarverma_2k21cse02@dtu.ac.in>
2022-02-13 02:46:14 +0530

    Add Program 2

---
+++ 2_StatementAndFunction/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int addNumbers(int first_param, int second_param) {
+    int result = first_param + second_param;
+    return result;
+}
+
+int main() {
+    int first_number {3}; //statement
+    int second_number {7};
+
+    std::cout << "First Number : " << first_number << std::endl;
+    std::cout << "Second Number : " << second_number << std::endl;
+
+    int sum = first_number + second_number;
+    std::cout << "Sum : " << sum << std::endl;
+
+    sum = addNumbers(25, 7);
+    std::cout << "Sum : " << sum << std::endl;
+    return 0;
+}
