53e393b10bf4501129a750ebf96545940f9db0a9
Ayush Kumar Verma <ayushkumarverma_2k21cse02@dtu.ac.in>
2022-02-15 00:56:25 +0530

    Add Outformatting Program

---
+++ 6_OutputFormatting/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello";
+    std::cout << "World";
+
+    std::cout << std::endl;
+
+    std::cout << "------------" << std::endl;
+
+    std::cout << "Hello" << std::endl;
+    std::cout << "World" << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Hello\n";
+    std::cout << "World\n";
+    
+    std::cout << std::endl;
+
+    //==============================================================
+
+    std::cout << "Unformatted table : " << std::endl;
+    std::cout << "Daniel" << " " << "Gray" << " 25" << std::endl;
+    std::cout << "Stanley" << " " << "Woods" << " 33" << std::endl;
+    std::cout << "Jordan" << " " << "Parker" << " 45" << std::endl;
+    std::cout << "Joe" << " " << "Ball" << " 21" << std::endl;
+    std::cout << "Josh" << " " << "Carr" << " 27" << std::endl;
+
+    std::cout << std::endl;
+
+    //=========================================================================
+    
+    return 0;
+}
